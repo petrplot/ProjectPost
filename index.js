f8944a0c34d6888d531c5c8c41a52fdf7e0e04f3
petrplot <petr.plotnikov.1989@mail.ru>
2022-01-09 15:08:59 +0700

    conf add

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = require('./routes')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./config/config.json')
 
 
 const app = express()
 
 
-const dbUri  = 'mongodb+srv://petr:1234@cluster0.784l3.mongodb.net/projectPost?retryWrites=true&w=majority'
+const dbUri  = config.DB_URL
 const PORT = 5000
 
 app.use(express.json())
